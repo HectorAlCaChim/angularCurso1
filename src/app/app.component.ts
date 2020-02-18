0353dfcca57cc2d9390854a4d4a40c980e41fcfb
Hector <hectoralfonso_ hollow@hotmail.com>
2020-02-18 11:58:11 -0600

    configuracion de practica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angularCurso1';
+  numero1:number =0;
+  numero2:number =0;
+  resultado : number =0;
+  //valores de entrada y el tipo, por consiguiente que te devuelve
+  public sumaValores(numero1: number, numero2: number):number
+  {
+    this.resultado = this.numero1 + this.numero2;
+    return this.resultado;
+  }
 }
